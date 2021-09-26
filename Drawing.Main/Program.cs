c986a2bd782a1472504718830521ca8be0dd2b61
mark villanueva <mjyv15@gmail.com>
2021-09-26 23:17:37 +0800

    Add project files.

---
+++ Drawing.Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Drawing.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
